daba49564c865376826f7b6ad663999551bc3c2d
MaxMilone <54643189+MaxMilone@users.noreply.github.com>
2021-04-12 19:29:32 -0400

    Update skigate.vhd

--- Project/skigate.vhd
+++ Project/skigate.vhd
@@ -17,106 +17,86 @@ ENTITY skigate IS
 END skigate;
 
 ARCHITECTURE Behavioral OF skigate IS
-    CONSTANT gatesize : INTEGER := 8; -- gate size in pixels
-    CONSTANT ski_w : INTEGER := 5; -- bat width in pixels
-    CONSTANT ski_h : INTEGER := 5; -- bat height in pixels
-    -- distance ball moves each frame
+    CONSTANT gate_w : INTEGER := 25; -- gate width in pixels
+    CONSTANT gate_h : INTEGER := 3; -- gate height in pixels
+    CONSTANT ski_w : INTEGER := 5; -- player width in pixels
+    CONSTANT ski_h : INTEGER := 5; -- player height in pixels
+    -- distance gate descends each frame
     CONSTANT gate_speed : STD_LOGIC_VECTOR (10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR (6, 11);
-    SIGNAL gate_on : STD_LOGIC; -- indicates whether ball is at current pixel position
-    SIGNAL ski_on : STD_LOGIC; -- indicates whether bat at over current pixel position
+    SIGNAL gate_on : STD_LOGIC; -- indicates whether gate is at current pixel position
+    SIGNAL ski_on : STD_LOGIC; -- indicates whether player at over current pixel position
     SIGNAL game_on : STD_LOGIC := '0'; -- indicates whether gate is in motion
 
     -- current gate position - intitialized to top of screen
+-- NEED TO randomize this x position
     SIGNAL gate_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
-    SIGNAL gate_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
+    SIGNAL gate_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(600, 11);
 
-    -- bat vertical position
-    CONSTANT bat_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(500, 11);
-    -- current ball motion - initialized to (+ ball_speed) pixels/frame in both X and Y directions
-    SIGNAL ball_x_motion, ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := ball_speed;
+    -- player vertical position
+-- CHANGE THIS TO TOP OF SCREEN
+    CONSTANT bat_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(50, 11);
+    -- current gate motion - initialized to (+ gate_speed) pixels/frame in both X and Y directions
+    SIGNAL gate_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := gate_speed;
 BEGIN
-    red <= NOT bat_on; -- color setup for red ball and cyan bat on white background
-    green <= NOT ball_on;
-    blue <= NOT ball_on;
-    -- process to draw round ball
-    -- set ball_on if current pixel address is covered by ball position
-    balldraw : PROCESS (ball_x, ball_y, pixel_row, pixel_col) IS
+    red <= NOT ski_on; -- color setup for red ball and cyan bat on white background
+    green <= NOT gate_on;
+    blue <= NOT gate_on;
+    -- process to draw gate
+    -- set gate_on if current pixel address is covered by gate position
+    gatedraw : PROCESS (gate_x, gate_y, pixel_row, pixel_col) IS
         VARIABLE vx, vy : STD_LOGIC_VECTOR (10 DOWNTO 0); -- 9 downto 0
     BEGIN
-        IF pixel_col <= ball_x THEN -- vx = |ball_x - pixel_col|
-            vx := ball_x - pixel_col;
+        IF pixel_row <= gate_y THEN -- vy = |ball_y - pixel_row|
+            vy := gate_y - pixel_row;
         ELSE
-            vx := pixel_col - ball_x;
-        END IF;
-        IF pixel_row <= ball_y THEN -- vy = |ball_y - pixel_row|
-            vy := ball_y - pixel_row;
-        ELSE
-            vy := pixel_row - ball_y;
-        END IF;
-        IF ((vx * vx) + (vy * vy)) < (bsize * bsize) THEN -- test if radial distance < bsize
-            ball_on <= game_on;
-        ELSE
-            ball_on <= '0';
+            vy := pixel_row - gate_y;
         END IF;
     END PROCESS;
-    -- process to draw bat
-    -- set bat_on if current pixel address is covered by bat position
-    batdraw : PROCESS (bat_x, pixel_row, pixel_col) IS
+            
+    -- process to draw player/skiier
+    -- set ski_on if current pixel address is covered by player position
+    skidraw : PROCESS (ski_x, pixel_row, pixel_col) IS
         VARIABLE vx, vy : STD_LOGIC_VECTOR (10 DOWNTO 0); -- 9 downto 0
     BEGIN
-        IF ((pixel_col >= bat_x - bat_w) OR (bat_x <= bat_w)) AND
-         pixel_col <= bat_x + bat_w AND
-             pixel_row >= bat_y - bat_h AND
-             pixel_row <= bat_y + bat_h THEN
-                bat_on <= '1';
+        IF ((pixel_col >= ski_x - ski_w) OR (ski_x <= ski_w)) AND
+         pixel_col <= ski_x + ski_w AND
+             pixel_row >= ski_y - ski_h AND
+             pixel_row <= ski_y + ski_h THEN
+                ski_on <= '1';
         ELSE
-            bat_on <= '0';
+            ski_on <= '0';
         END IF;
     END PROCESS;
-    -- process to move ball once every frame (i.e. once every vsync pulse)
-    mball : PROCESS
+            
+    -- process to move gate once every frame (i.e. once every vsync pulse)
+    mgate : PROCESS
         VARIABLE temp : STD_LOGIC_VECTOR (11 DOWNTO 0);
     BEGIN
         WAIT UNTIL rising_edge(v_sync);
-        IF serve = '1' AND game_on = '0' THEN -- test for new serve
+        IF start = '1' AND game_on = '0' THEN -- test for new game start
             game_on <= '1';
-            ball_y_motion <= (NOT ball_speed) + 1; -- set vspeed to (- ball_speed) pixels
-        ELSIF ball_y <= bsize THEN -- bounce off top wall
-            ball_y_motion <= ball_speed; -- set vspeed to (+ ball_speed) pixels
-        ELSIF ball_y + bsize >= 600 THEN -- if ball meets bottom wall
-            ball_y_motion <= (NOT ball_speed) + 1; -- set vspeed to (- ball_speed) pixels
+            gate_y_motion <= (NOT gate_speed) - 1; -- set vspeed to (- ball_speed) pixels
+        ELSIF gate_y + gate_h >= 600 THEN -- if ball meets bottom wall
+            gate_y_motion <= (NOT gate_speed) + 1; -- set vspeed to (- ball_speed) pixels
             game_on <= '0'; -- and make ball disappear
         END IF;
-        -- allow for bounce off left or right of screen
-        IF ball_x + bsize >= 800 THEN -- bounce off right wall
-            ball_x_motion <= (NOT ball_speed) + 1; -- set hspeed to (- ball_speed) pixels
-        ELSIF ball_x <= bsize THEN -- bounce off left wall
-            ball_x_motion <= ball_speed; -- set hspeed to (+ ball_speed) pixels
-        END IF;
-        -- allow for bounce off bat
-        IF (ball_x + bsize/2) >= (bat_x - bat_w) AND
-         (ball_x - bsize/2) <= (bat_x + bat_w) AND
-             (ball_y + bsize/2) >= (bat_y - bat_h) AND
-             (ball_y - bsize/2) <= (bat_y + bat_h) THEN
-                ball_y_motion <= (NOT ball_speed) + 1; -- set vspeed to (- ball_speed) pixels
+        -- if player is in between the gates, spawn new gate
+        IF (gate_x + gate_w/2) >= (ski_x - ski_w) AND
+         (gate_x - gate_w/2) <= (ski_x + ski_w) AND
+             (gate_y + gate_w/2) >= (ski_y - ski_h) AND
+             (gate_y - gate_w/2) <= (ski_y + ski_h) THEN
+                 SIGNAL gate_x : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
+                 SIGNAL gate_y : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(600, 11);
         END IF;
         -- compute next ball vertical position
         -- variable temp adds one more bit to calculation to fix unsigned underflow problems
         -- when ball_y is close to zero and ball_y_motion is negative
-        temp := ('0' & ball_y) + (ball_y_motion(10) & ball_y_motion);
+        temp := ('0' & gate_y) + (gate_y_motion(10) & gate_y_motion);
         IF game_on = '0' THEN
-            ball_y <= CONV_STD_LOGIC_VECTOR(440, 11);
+            gate_y <= CONV_STD_LOGIC_VECTOR(440, 11);
         ELSIF temp(11) = '1' THEN
-            ball_y <= (OTHERS => '0');
-        ELSE ball_y <= temp(10 DOWNTO 0); -- 9 downto 0
-        END IF;
-        -- compute next ball horizontal position
-        -- variable temp adds one more bit to calculation to fix unsigned underflow problems
-        -- when ball_x is close to zero and ball_x_motion is negative
-        temp := ('0' & ball_x) + (ball_x_motion(10) & ball_x_motion);
-        IF temp(11) = '1' THEN
-            ball_x <= (OTHERS => '0');
-        ELSE ball_x <= temp(10 DOWNTO 0);
+            gate_y <= (OTHERS => '0');
+        ELSE gate_y <= temp(10 DOWNTO 0); -- 9 downto 0
         END IF;
     END PROCESS;
 END Behavioral;
