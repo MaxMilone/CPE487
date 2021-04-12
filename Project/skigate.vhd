cafce9879c883a7d4abaf6cec794d03c49911978
MaxMilone <54643189+MaxMilone@users.noreply.github.com>
2021-04-12 11:47:18 -0400

    Update skigate.vhd

--- Project/skigate.vhd
+++ Project/skigate.vhd
@@ -18,38 +18,38 @@ END skigate;
 
 ARCHITECTURE Behavioral OF skigate IS
 	CONSTANT size  : INTEGER := 8;
-	SIGNAL ball_on : STD_LOGIC; -- indicates whether ball is over current pixel position
-	-- current ball position - intitialized to center of screen
-	SIGNAL ball_x  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
-	SIGNAL ball_y  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
-	-- current ball motion - initialized to +4 pixels/frame
-	SIGNAL ball_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000100";
+	SIGNAL gate_on : STD_LOGIC; -- indicates whether the gate is over current pixel position
+	-- current gate position - intitialized to center of screen
+	SIGNAL gate_x  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(400, 11);
+	SIGNAL gate_y  : STD_LOGIC_VECTOR(10 DOWNTO 0) := CONV_STD_LOGIC_VECTOR(300, 11);
+	-- current gate motion - initialized to +4 pixels/frame
+	SIGNAL gate_y_motion : STD_LOGIC_VECTOR(10 DOWNTO 0) := "00000000100";
 BEGIN
-	red <= '1'; -- color setup for red ball on white background
-	green <= NOT ball_on;
-	blue  <= NOT ball_on;
-	-- process to draw ball current pixel address is covered by ball position
-	bdraw : PROCESS (ball_x, ball_y, pixel_row, pixel_col) IS
+	red <= '1'; -- color setup for red gate on white background
+	green <= NOT gate_on;
+	blue  <= NOT gate_on;
+	-- process to draw gate current pixel address is covered by gate position
+	gdraw : PROCESS (gate_x, gate_y, pixel_row, pixel_col) IS
 	BEGIN
-		IF (pixel_col >= ball_x - size) AND
-		 (pixel_col <= ball_x + size) AND
-			 (pixel_row >= ball_y - size) AND
-			 (pixel_row <= ball_y + size) THEN
-				ball_on <= '1';
+		IF (pixel_col >= gate_x - size) AND
+		 (pixel_col <= gate_x + size) AND
+			 (pixel_row >= gate_y - size) AND
+			 (pixel_row <= gate_y + size) THEN
+				gate_on <= '1';
 		ELSE
-			ball_on <= '0';
+			gate_on <= '0';
 		END IF;
 		END PROCESS;
-		-- process to move ball once every frame (i.e. once every vsync pulse)
-		mball : PROCESS
+		-- process to move the gate once every frame (i.e. once every vsync pulse)
+		mgate : PROCESS
 		BEGIN
 			WAIT UNTIL rising_edge(v_sync);
 			-- allow for bounce off top or bottom of screen
-			IF ball_y + size >= 600 THEN
-				ball_y_motion <= "11111111100"; -- -4 pixels
-			ELSIF ball_y <= size THEN
-				ball_y_motion <= "00000000100"; -- +4 pixels
+			IF gate_y + size >= 600 THEN
+				gate_y_motion <= "11111111100"; -- -4 pixels
+			--ELSIF gate_y <= size THEN
+				--gate_y_motion <= "00000000100"; -- +4 pixels
 			END IF;
-			ball_y <= ball_y + ball_y_motion; -- compute next ball position
+			gate_y <= gate_y + gate_y_motion; -- compute next gate position
 		END PROCESS;
 END Behavioral;
